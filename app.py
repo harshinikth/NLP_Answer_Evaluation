608b3c37963051ad7775020a8c5c05f38ac7cba1
harshinikth <harshinikth@gmail.com>
2026-06-28 16:46:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -212,16 +212,7 @@ elif user_type == "Student":
     if st.session_state.exam_started and not st.session_state.exam_submitted:
         st.header(f"📋 Exam for {st.session_state.student_name} - {st.session_state.student_regno}")
         st.progress(0.0)
-    answered = sum(
-        1 for ans in st.session_state.student_answers.values()
-        if ans.strip()
-    )
-
-    total_q = len(st.session_state.selected_questions)
-    progress = answered / total_q if total_q > 0 else 0
-    st.progress(progress)
-
-    st.write(f"### Progress: {answered}/{total_q} Questions Answered")
+    
     st.warning("⚠️ Do not refresh the page. Answer all questions and submit.")
 
     with st.form("exam_form"):
